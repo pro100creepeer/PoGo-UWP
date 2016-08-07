c39fb65e69ad9b7588d980ca345c122b3281bf38
WallyCZE <vlipert@seznam.cz>
2016-08-07 15:06:43 +0200

    added encrypt module & signature proto

---
+++ PokemonGoAPI/Helpers/SignatureGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGoAPI.Helpers
+{
+    class SignatureGenerator
+    {
+        private static Random rnd = new Random();
+
+        public static byte[] GetSignature(byte[] input)
+        {
+            byte[] iv = new byte[16];
+
+            rnd.NextBytes(iv);
+
+            uint outsize = PokemonGoAPI.Encrypter.GetOutputSize(input, iv);
+
+            byte[] output = new byte[outsize];
+
+            PokemonGoAPI.Encrypter.Encrypt(input, iv, output);
+
+            return output;
+        }
+    }
+}
